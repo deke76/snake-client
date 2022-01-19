d2c1a197f799b7119097dc262b56383712d4f61e
Darcy <darcy.lauder@shaw.ca>
2022-01-19 10:00:22 -0700

    separated connect from play

--- play.js
+++ play.js
@@ -1,22 +1,8 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: '50541'
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
+const { connect } = require('./client.js')
 
 console.log("Connecting ...");
 let conn = connect();
-
-conn.setEncoding('utf8'); // interpret data as text
 
 conn.on('data', (data) => {
   console.log('Server says: ', data);
